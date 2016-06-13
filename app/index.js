10b4a010d127067dfb4a97eac6e6baf6ecaab34f
balibou <benjamin.cherion@gmail.com>
2016-06-13 15:58:51 +0200

    adding routes

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+'use strict';
 
+import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
@@ -13,13 +14,6 @@ class GymApp extends Component {
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
       </View>
     );
   }
@@ -31,16 +25,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
 });
 
-module.exports = GymApp;
+export default GymApp;
--- index.ios.js
+++ index.ios.js
@@ -1,6 +1,29 @@
-'use strict';
-
 import { AppRegistry } from 'react-native';
 import GymApp from './app';
 
-AppRegistry.registerComponent('GymApp', () => GymApp);
+import React,{ Component } from 'react';
+import {
+  StyleSheet,
+  NavigatorIOS
+} from 'react-native';
+
+class GymAppRoute extends Component {
+  render() {
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'GymApp',
+          component: GymApp,
+        }}/>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+AppRegistry.registerComponent('GymApp', () => GymAppRoute);
